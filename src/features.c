bbe1cf97a0ca908f38eccf91ccd17d191b32027e
PIE-FOU <pierre.fournier@etu.estia.fr>
2025-05-21 17:39:36 +0200

    en cours debug first pixel

--- src/features.c
+++ src/features.c
@@ -32,8 +32,9 @@ void dimension(char *chemin) {
 
 void first_pixel() {
     int read_image_data(const char *filename, unsigned char **data, int *width, int *height, int *channel_count);
-    if ( strncmp( configuration.command, "first_pixel", 11 ) == 0 ) {/* first_pixel() function is defined in feature.h and implemented in feature.c */
+    if ( strncmp( read_image_data, "first_pixel", 11 ) == 0 ) {/* first_pixel() function is defined in feature.h and implemented in feature.c */
     
-    first_pixel();
+    printf("first_pixel :")first_pixel();
+
 }
 }
