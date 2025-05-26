b9f6c19740ad44140f1c33f523d1344fc3bf3fe3
PIE-FOU <pierre.fournier@etu.estia.fr>
2025-05-26 12:18:47 +0200

    modification balisage

--- src/features.c
+++ src/features.c
@@ -5,7 +5,6 @@
 #include "features.h"
 #include "utils.h"
 
-
 /**
  * @brief Here, you have to code features of the project.
  * Do not forget to commit regurlarly your changes.
@@ -13,84 +12,40 @@
  * When the feature is totally implemented, your commit message must contain "close #n".
  */
 
-void helloWorld() {
+void helloWorld()
+{
     printf("Hello World !");
 }
 
-void dimension(char *chemin) {
+void dimension(char *chemin)
+{
     unsigned char *donnees;
     int largeur;
     int hauteur;
     int nb_canaux;
-    
+
     read_image_data(chemin, &donnees, &largeur, &hauteur, &nb_canaux);
-    
+
     printf("dimension: %d, %d\n", largeur, hauteur);
-    
 }
 /* debut zone fisrt_pixel */
-void first_pixel() {
+void first_pixel()
+{
     int read_image_data(const char *filename, unsigned char **data, int *width, int *height, int *channel_count);
-    if ( strncmp( read_image_data, "first_pixel", 11 ) == 0 ) {/* first_pixel() function is defined in feature.h and implemented in feature.c */
-    
-    printf("first_pixel :");
-    first_pixel();
+    if (strncmp(read_image_data, "first_pixel", 11) == 0)
+    { /* first_pixel() function is defined in feature.h and implemented in feature.c */
 
+        printf("first_pixel :");
+        first_pixel();
+    }
+
+    /* fin zone fisrt_pixel */
+
+    /* debut zone Color of the 10th pixel #5 */
+
+    /* fin zone  Color of the 10th pixel #5 */
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* fin zone fisrt_pixel */
-
-
-/* debut zone Color of the 10th pixel #5 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* fin zone  Color of the 10th pixel #5 */
-
+void tenth_pixel(char *source_path)
+{
 }
